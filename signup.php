0b0e4eae7c5bfc3fe6e82e7424b0118acc0134ac
Mohammed Jafar Sadik <jafar.sadik@northsouth.edu>
2018-12-16 10:50:51 +0600

    Php file with encryption function

--- signup.php
+++ signup.php
@@ -1,4 +1,5 @@
 <?php
+$username = $_POST['username'];
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $gender = $_POST['gender'];
@@ -10,8 +11,9 @@ $country = $_POST['country'];
 $zip = $_POST['zip'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+//$param_password = password_hash($password, PASSWORD_DEFAULT);
 
-if(!empty($first_name) || !empty($last_name) || !empty($gender) || !empty($phone) || !empty($address) || !empty($city) || !empty($state) || !empty($country) || !empty($zip) || !empty($email) || !empty($password))
+if( !empty($username) || !empty($first_name) || !empty($last_name) || !empty($gender) || !empty($phone) || !empty($address) || !empty($city) || !empty($state) || !empty($country) || !empty($zip) || !empty($email) || !empty($password))
 {
     $host = "localhost";
     $dbUsername = "root";
@@ -23,39 +25,50 @@ if(!empty($first_name) || !empty($last_name) || !empty($gender) || !empty($phone
 
     if(mysqli_connect_error())
     {
-    	die('Connect Error('. mysqli_connect_errno(). ')' . mysqli_connect_error());
+        die('Connect Error('. mysqli_connect_errno(). ')' . mysqli_connect_error());
     }
 
     else
     {
-    	$SELECT = "SELECT email from user_info where email = ? Limit 1";
-    	$INSERT = "INSERT into user_info (first_name, last_name, gender, phone, address, city, state, country, zip, email, password) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        //$SELECT = "SELECT email from user_info where email = ? Limit 1";
+        $SELECT= "SELECT username from user_info where username = ? Limit 1";
+        $password = password_hash($password, PASSWORD_DEFAULT); 
+        $INSERT = "INSERT into user_info (username, first_name, last_name, gender, phone, address, city, state, country, zip, email, password) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-    	$stmt = $conn->prepare($SELECT);
-    	$stmt->bind_param("s", $email);
-    	$stmt->execute();
-    	$stmt->bind_result($email);
-    	$stmt->store_result();
-    	$rnum = $stmt->num_rows;
+        /*$stmt = $conn->prepare($SELECT);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->bind_result($email);
+        $stmt->store_result();
+        $rnum = $stmt->num_rows;*/
 
-    	if($rnum == 0)
-    	{
-    		$stmt->close();
-    		$stmt = $conn->prepare($INSERT);
-    		$stmt->bind_param("sssissssiss", $first_name, $last_name, $gender, $phone, $address, $city, $state, $country, $zip, $email, $password);
-    		$stmt-> execute();
-    		
-    		header('Location: login.html');
-    		//echo "New Record inserted successfully";
+        $stmt = $conn->prepare($SELECT);
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $stmt->bind_result($username);
+        $stmt->store_result();
+        $rnum = $stmt->num_rows;
 
-    	}
+        if($rnum == 0)
+        {
+            $stmt->close();
+            $stmt = $conn->prepare($INSERT);
+            $stmt->bind_param("ssssissssiss", $username, $first_name, $last_name, $gender, $phone, $address, $city, $state, $country, $zip, $email, $password);
+            
+           // $param_username = $username;
+            
+            $stmt-> execute();
+            header('Location: index.html');
+            //echo "New Record inserted successfully";
 
-    	else
-    	{
-    		echo "Somebody already registered using this email";
-    	}
-    	$stmt->close();
-    	$conn->close(); 
+        }
+
+        else
+        {
+            echo "Somebody already registered using this username. Please try a new username";
+        }
+        $stmt->close();
+        $conn->close(); 
 
 
 
@@ -66,8 +79,8 @@ if(!empty($first_name) || !empty($last_name) || !empty($gender) || !empty($phone
 
 else
 {
-	echo "All fields are required";
-	die();
+    echo "All fields are required";
+    die();
 }
 
 
